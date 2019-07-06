16ce42807bbcf7f6036190627076b2e7da2040ed
lequocgiom <letrung95@gmail.com>
2019-07-06 10:29:17 +0700

    first commit: adding assets, create script for player movement

---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5, xBounds = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float hor = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float ver = speed * Time.deltaTime;
+
+        transform.Translate(new Vector2(hor, 0));
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -xBounds, xBounds),
+            transform.position.y);
+    }
+}
